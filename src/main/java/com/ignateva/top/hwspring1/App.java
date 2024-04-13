cc3f76364a20c0f68cebbbc5a7e9c172e749e245
Мария <maryig77@yandex.ru>
2024-04-13 20:50:42 +0300

    java code

--- src/main/java/com/ignateva/top/hwspring1/App.java
+++ src/main/java/com/ignateva/top/hwspring1/App.java
@@ -1,6 +1,7 @@
 package com.ignateva.top.hwspring1;
 
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
@@ -8,10 +9,11 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-       ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+       AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         Language eng = context.getBean("english", Language.class);
         Language ital = context.getBean("italian", Language.class);
         Language germ = context.getBean("german", Language.class);
+
        DayMaker dayMaker= new DayMaker(eng,ital,germ);
        int day = dayMaker.getTodaysDayOfWeek();
         Scanner scan = new Scanner(System.in);
---
+++ src/main/java/com/ignateva/top/hwspring1/SpringConfig.java
@@ -0,0 +1,11 @@
+package com.ignateva.top.hwspring1;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan("com.ignateva.top.hwspring1")
+public class SpringConfig {
+
+}
